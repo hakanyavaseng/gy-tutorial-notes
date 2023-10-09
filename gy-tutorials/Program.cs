771b83ab3cecdec9b942607d8dba7e8675cff191
Hakan Yavaş <hakanyavaseng@gmail.com>
2023-10-09 20:06:11 +0300

    resource-optimization with try-catch

--- gy-tutorials/Program.cs
+++ gy-tutorials/Program.cs
@@ -10,21 +10,22 @@ namespace tutorial
 
             #region try - catch
 #if true
+            Console.WriteLine("Please enter number 1: ");
+            int num1 = default, num2= default;
             try
             {
                 //Contains possible runtime errors.
-                Console.WriteLine("Please enter number 1: ");
-                int num1 = int.Parse(Console.ReadLine());
+                num1 = int.Parse(Console.ReadLine());
                 Console.WriteLine("Please enter number 2: ");
-                int num2 = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Sum: " + (num1 + num2));
+                num2 = int.Parse(Console.ReadLine());
 
             }
             catch
             {
                 Console.WriteLine("Please enter number.");
             }
+
+            Console.WriteLine("Sum: " + (num1 + num2));
 
 #endif
 
