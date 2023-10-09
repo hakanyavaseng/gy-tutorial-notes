7bb472a2527be93ad312752a68323b2110e47531
Hakan Yavaş <hakanyavaseng@gmail.com>
2023-10-09 19:59:43 +0300

    try-catch

---
+++ gy-tutorials/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace tutorial
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region try - catch
+#if true
+            try
+            {
+                //Contains possible runtime errors.
+                Console.WriteLine("Please enter number 1: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter number 2: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Sum: " + (num1 + num2));
+
+            }
+            catch
+            {
+                Console.WriteLine("Please enter number.");
+            }
+
+#endif
+
+            #endregion
+
+
+
+        }
+
+
+    }
+
+
+}
