7ed10ec87de2c40bf58def34791f708e274db029
Hakan Yavaş <hakanyavaseng@gmail.com>
2023-10-12 16:22:41 +0300

    for loop variations

--- loops/Program.cs
+++ loops/Program.cs
@@ -8,7 +8,32 @@ namespace tutorial
         static void Main(string[] args)
         {
 
-         
+            int i = default;
+
+            #region for variations
+            for(int j = 0; j < args.Length; j++) {  }
+
+            for(; i < args.Length; i++) { }
+
+            for(i = 5; i < args.Length; i++) { } //If you want to use pre declared variable inside for loop, you must assign again.
+
+            for(; i < args.Length; ) { i++; }
+
+            string str = "try";
+            for(i=5; str == "ntry"; i++) { } 
+
+            for(; ; ) { break; } // Infinite loop
+
+            for(int j = 0 , k = 10; j <10 && k > 10; j++, k++) { }
+
+
+
+
+
+            #endregion
+
+           
+
 
 
         }
