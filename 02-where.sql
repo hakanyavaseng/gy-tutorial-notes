e529180510c018e58c50636a1c3ca6cf3c4a8ae6
Hakan Yavas <hakanyavaseng@gmail.com>
2023-10-18 13:33:06 +0300

    additional operators

--- 02-where.sql
+++ 02-where.sql
@@ -12,4 +12,9 @@ Select * from Personeller where Sehir = 'London' and Ulke = 'UK'
 -- or operator 
 Select * from Personeller where UnvanEki = 'Mr.' or Sehir = 'Seattle'
 
+-- <> Not equal
+-- = equal
+-- <= less than or equal to
+-- >= greater than or equal to 
 
+Select * from Personeller where Bolge <> 'NULL'
