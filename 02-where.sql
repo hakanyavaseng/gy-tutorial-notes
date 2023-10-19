c3fc3792c27172102b765fa74c9c0542355507cf
Hakan Yavas <hakanyavaseng@gmail.com>
2023-10-19 12:08:36 +0300

    usage of between command

--- 02-where.sql
+++ 02-where.sql
@@ -18,3 +18,7 @@ Select * from Personeller where UnvanEki = 'Mr.' or Sehir = 'Seattle'
 -- >= greater than or equal to 
 
 Select * from Personeller where Bolge <> 'NULL'
+
+-- WHERE & BETWEEN
+
+Select * from Personeller where YEAR(DogumTarihi) between 1950 and 1965
