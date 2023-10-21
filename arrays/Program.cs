b41b50a802634d5806d94dc16873d30d889d4bb4
Hakan Yavaş <hakanyavaseng@gmail.com>
2023-10-21 13:52:22 +0300

    basic

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -8,6 +8,16 @@ namespace arrays
         static void Main(string[] args)
         {
 
+            #region Arrays
+
+            int[] age = new int[5]; // Age referans turlu olup stackte tutulur, heap'teki degerleri referans eder
+
+            
+
+
+
+
+            #endregion
 
         }
 
