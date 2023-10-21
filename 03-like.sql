80ec1e614d5ef3f2a964e951b5cfd802c4640694
Hakan Yavaş <hakanyavaseng@gmail.com>
2023-10-21 09:46:29 +0300

    like query

---
+++ 03-like.sql
@@ -0,0 +1,42 @@
+USE Northwind
+
+--Like 
+
+-- % operator 
+
+Select Adi,Soyadi from Personeller where Adi like 'j%'
+
+Select Adi,Soyadi from Personeller where Adi like '%y'
+
+Select * from Personeller where adi like '%ert'
+
+Select * from Personeller where adi like 'r%t'
+
+-- unnecessary usage
+Select * from Personeller where adi like 'r%' and adi like '%t'
+
+Select * from Personeller where Adi like '%an%'
+
+Select * from Personeller where adi like 'n%an%'
+
+-- _ operator
+
+Select * from Personeller where adi like 'a_n%' 
+
+Select * from Personeller where adi like 'm___a%'
+
+-- [] (ya da) operator
+
+Select * from Personeller where adi like '[nmr]%' -- Brings employees which their first name's first char n or m or r 
+
+Select * from Personeller where adi like '%[ai]%' 
+
+-- [*-*] operator (alphabet)
+
+Select adi from Personeller where adi like '[a-k]%'
+
+-- [^*] operator (degil operatoru)
+
+Select * from Personeller where adi like '[^a]%'
+
+Select * from Personeller where adi like '[^an]%'
