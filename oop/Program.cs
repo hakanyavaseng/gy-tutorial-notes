711f340d6981f3108e9450143acaa7383e7cbfe2
Hakan Yavas <hakanyavaseng@gmail.com>
2023-11-09 18:30:09 +0300

    stack-heap-reference object relation

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,18 +1,23 @@
 ﻿//GY-OOP-TUTORIALS
 namespace oop
 {
-
     class Program
     {
         static void Main(string[] args)
         {
             //myClass o1 = new myClass();
             myClass o1 = new(); // C# 9.0
+            o1.MyProperty = 1;
 
+            myClass o2 = null; //Has reference on stack but null on heap
+            //o2.MyProperty = 20; Object reference not set to an instance of an object.
+
+
+            new myClass(); //Referansla isaretlenmedigi icin, bu nesneye artık erisilemez. 
         }
     }
     class myClass
     {
-
+        public int MyProperty { get; set; }
     }
 }
