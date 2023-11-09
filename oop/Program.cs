219ab2bb99dda02bde3fe36058bf0b492d94bf78
Hakan Yavas <hakanyavaseng@gmail.com>
2023-11-09 17:38:12 +0300

    c# 9.0, class initialization

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,1 +1,18 @@
 ﻿//GY-OOP-TUTORIALS
+namespace oop
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //myClass o1 = new myClass();
+            myClass o1 = new(); // C# 9.0
+
+        }
+    }
+    class myClass
+    {
+
+    }
+}
