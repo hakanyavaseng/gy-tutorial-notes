b63e1ed0bfd0c85bd802646b194ff820c1051cca
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-01-27 16:30:13 +0300

    2701, saving related data

---
+++ 25_Saving-Related-Data/Migrations/20240127132726_mig-4.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _25_Saving_Related_Data.Migrations
+{
+    public partial class mig4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Books_BooksId",
+                table: "AuthorBook");
+
+            migrationBuilder.RenameColumn(
+                name: "BooksId",
+                table: "AuthorBook",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorsId",
+                table: "AuthorBook",
+                newName: "AuthorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBook_BooksId",
+                table: "AuthorBook",
+                newName: "IX_AuthorBook_BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorId",
+                table: "AuthorBook",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Books_BookId",
+                table: "AuthorBook",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Books_BookId",
+                table: "AuthorBook");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "AuthorBook",
+                newName: "BooksId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "AuthorBook",
+                newName: "AuthorsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBook_BookId",
+                table: "AuthorBook",
+                newName: "IX_AuthorBook_BooksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook",
+                column: "AuthorsId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Books_BooksId",
+                table: "AuthorBook",
+                column: "BooksId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
