839a673e91be104374d2bf4d9da97411ba7dce73
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-21 12:05:56 +0300

    21/02, ref return and ref local

---
+++ Advanced-C#-Programming/02_Ref-Return&Ref-Local/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Ref Keyword
+
+using System.Xml.Linq;
+
+int b = 5;
+
+Console.WriteLine(b);
+
+X(ref b);
+
+Console.WriteLine(b);
+
+void X(ref int _b)
+{
+    _b++;
+}
+#endregion
+
+#region Ref Return
+//Performans gerektiren durumlarda kodu optimize etmek ve gereksiz değişken tanımları ve veri tekrarını engellemek için oldukça faydalıdır.
+ref int Y(ref int _b)
+{
+    _b = 124;
+    return ref _b;
+}
+#endregion
+
+#region Ref Local
+int x = 10;
+ref int y = ref x;
+//x ve y aynı bellek adresini tutuyorlar.
+Console.WriteLine(x);
+Console.WriteLine(y);
+x = 20;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+#endregion
