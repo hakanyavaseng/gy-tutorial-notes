e5d2901e21eafbd4b70dc5f16705895dd58152af
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-24 19:47:06 +0300

    2402, data concurrency file created

---
+++ EntityFramework-Core/59_Data-Concurrency/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+#region Data Concurrency Nedir?
+
+#endregion
+
+#region Stale & Dirty Data Nedir?
+
+#endregion
+
+#region Last In Wins
+
+#endregion
+
+#region Pessimistic Lock
+
+#endregion
+
+#region Optimistic Lock
+
+#endregion
+
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; } 
+}
+
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=DataConcurrencyDb;Trusted_Connection=true;TrustServerCertificate=True;");
+    }
+}
+
