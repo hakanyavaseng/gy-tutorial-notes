52ac2faaa8e6cb9ec22000370bc4130b0ccb8a12
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-17 12:02:44 +0300

    14, expando object.

--- Advanced-C#-Programming/13_Anonymous-Structures/Program.cs
+++ Advanced-C#-Programming/13_Anonymous-Structures/Program.cs
@@ -1,38 +1,35 @@
 ﻿using Microsoft.VisualBasic;
 
-
-#region Anonymous Structures Nedir?
-//Anonymous Type, Method, Collection, Property, Array ve Delegate gibi alt kavramlar mevcuttur.
-//İhtiyaç doğrultusunda ilgili yapılanmayı önceden tanımlamaya gerek kalmaksızın kullanmamıza olanak sağlar.
+#region What are Anonymous Structures?
+//Anonymous Type, Method, Collection, Property, Array, and Delegate are subconcepts.
+//It allows us to use the respective structuring without the need to define it in advance according to the need.
 
 /*
-    => Anonymous Objects (Types) : Anonymous Objects, derleme zamanında tanımlanmayan ve adlandırılmayan nesnelerdir. Anlık ve tek kullanımlık veri yapısı oluşturulması gereken durumlarda oldukça kullanışlıdır.
-    => Anonymous Methods : Bir isim vermeksizin bir metodun örneğini oluşturmamızı sağlayan özelliktir. 
-    => Anonymous Collections : Anonymous Collections, derleme zamanında tanımlanmayan ve adlandırılmayan koleksiyonlardır. 
+    => Anonymous Objects (Types): Anonymous Objects are objects that are not defined or named at compile time. They are useful when an instant and disposable data structure is needed.
+    => Anonymous Methods: A feature that allows us to create an instance of a method without naming it.
+    => Anonymous Collections: Anonymous Collections are collections that are not defined or named at compile time.
 */
 #endregion
 
 #region Anonymous Objects (Types)  
-//var keywordu ile tanımlanır, çünkü isim ve türü belli olmadığından compiler tarafından türü belirlenir.
+//It is defined with the var keyword because its name and type are unknown, so the type is determined by the compiler.
 
-
-var anonymousObject = new 
-{ 
-    Name = "Hakan",
-    Surname = "Yavaş",
-    Age = 22 
+var anonymousObject = new
+{
+    Name = "Hakan", // (Name property)
+    Surname = "Yavaş", // (Surname property)
+    Age = 22 // (Age property)
 };
 
-//anonymousObject.Name = "Alperen"; //Hata verir, çünkü readonly'dir.
+//anonymousObject.Name = "Alperen"; //Produces an error because it is readonly.
 #endregion
 
 #region Anonymous Methods
-//Anonymous Methods, genellikle delegate'lerle beraber kullanılmaktadır. 
-
+//Anonymous Methods are typically used with delegates.
 
 AddHandler add1 = new AddHandler((int a, int b) => a + b); //Anonymous Method
-AddHandler add2 = delegate(int a, int b) { return a + b; }; //Anonymous Method
-var add3 = (int a, int b) => a + b; //Anonymous Method (Compiler var kullanıldığı için hazır olarak bulunan bir delegate türüne dönüştürür. Bu durumda Func.)
+AddHandler add2 = delegate (int a, int b) { return a + b; }; //Anonymous Method
+var add3 = (int a, int b) => a + b; //Anonymous Method (Since var is used, it is converted to a predefined delegate type by the compiler. In this case, Func.)
 
 delegate int AddHandler(int num1, int num2);
 
@@ -45,8 +42,8 @@ var a = new[] { 1, 2, 3, 4, 5 }; //Array, int[]?
 //List
 var l = new Collection()
 {
-    new { Name = "Hakan", Surname = "Yavaş", Age = 22 },
-    new { Name = "Alperen", Surname = "Güneş", Age = 22 }
+    new { Name = "Hakan", Surname = "Yavaş", Age = 22 }, // (Anonymous object with Name, Surname, and Age properties)
+    new { Name = "Alperen", Surname = "Güneş", Age = 22 } // (Anonymous object with Name, Surname, and Age properties)
 };
 
 #endregion
---
+++ Advanced-C#-Programming/14_Expando-Object/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Dynamic;
+
+#region What is ExpandoObject?
+/*
+    => ExpandoObject is a class used in C# to create dynamically expandable and shapeable objects.
+    => With this class, we can shape an object at runtime, add, delete, or update properties and methods within it.
+    => The ExpandoObject class is found within the System.Dynamic namespace. Thus, it utilizes the dynamic type.
+    => Additionally, since it implements the IDictionary<string, object> interface, it stores dynamically defined properties.
+    => It can be used for creating dynamic data structures, adding dynamic properties, temporary data storage, dynamic application development, etc.
+*/
+#endregion
+
+#region Usage and Structure of ExpandoObject
+
+dynamic expandoObject = new ExpandoObject();
+
+expandoObject.Name = "Hakan";
+expandoObject.Surname = "Yavas";
+expandoObject.Age = 22;
+
+expandoObject.GetFullName = new Func<string>(() => expandoObject.Name + " " + expandoObject.Surname);
+
+//Console.WriteLine($"{expandoObject.Name}, {expandoObject.Surname}, {expandoObject.Age}");
+
+
+IDictionary<string, object> expandoDictionary = (IDictionary<string, object>)expandoObject;
+foreach (KeyValuePair<string, object> item in expandoDictionary)
+{
+    Console.WriteLine($"{item.Key} : {item.Value}");
+}
+
+#endregion
