78e19aa259fc26aa73b1349afe732b3797dda757
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-24 12:03:56 +0300

    24/03, SignalR basics and simple server and client application.

---
+++ SignalR-WebSocket/01_SignalR-Basics/01_SignalR-Basics/SignalR.Server/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Server.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("recevieMessage", message);
+        }
+    }
+}
