40bf7f3c052b5bf0450392629b09474d0450929c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-25 21:46:34 +0300

    Changing elements of collection during iteration causes exception examined.

---
+++ Advanced-C#-Programming/17_Custom-Iterational-Class-yield/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine();
+
+#region Changing Collection's Elements During Iteration - Exception
+List<int> numbers = Enumerable.Range(0,15).ToList();
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+    numbers.Add(100); // This will cause an exception because the collection is modified during iteration. 
+    // Error: : 'Collection was modified; enumeration operation may not execute.'
+}
+#endregion
+
+
