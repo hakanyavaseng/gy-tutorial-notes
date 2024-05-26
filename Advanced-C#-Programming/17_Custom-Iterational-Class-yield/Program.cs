01669713fc47c447969ee69fcae02ae985cc1ed6
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-05-26 07:46:39 +0300

    IEnumerable Interface

--- Advanced-C#-Programming/17_Custom-Iterational-Class-yield/Program.cs
+++ Advanced-C#-Programming/17_Custom-Iterational-Class-yield/Program.cs
@@ -15,6 +15,7 @@ foreach (int i in numbers)
 #endregion
 
 #region How to Make a Class Iterable 
+/*
 Stock stock = new();
 foreach (string material in stock) // Error: 'Stock' does not contain a definition for 'GetEnumerator' and no accessible extension method 'GetEnumerator' accepting a first argument of type 'Stock' could be found (are you missing a using directive or an assembly reference?)
 {
@@ -28,6 +29,27 @@ class Stock
     public void Add(string material) => materials.Add(material);
 
     public IEnumerator<string> GetEnumerator() // This method is required to make the class iterable
+    {
+        return materials.GetEnumerator();
+    }
+}
+*/
+#endregion
+
+#region IEnumerable Interface
+// IEnumerable interface is required to make a class iterable (foreach loop), it has only one method GetEnumerator() and there is also generic version IEnumerable<T>
+
+public class Stock : IEnumerable<string>
+{
+    List<string> materials = new() { "Wood", "Steel", "Plastic", "Glass" };
+    public void Add(string material) => materials.Add(material);
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return materials.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() // Non-generic version is implemented because object can be used in foreach loop
     {
         return materials.GetEnumerator();
     }
