6dcc32a149fa612178b1dddfc31e7fb93c04d28d
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-12 17:00:53 +0300

    Reflection with Assembly and Type types

---
+++ Advanced-C#-Programming/20_Reflection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+#region Reflection with Assembly type
+
+Assembly assembly = Assembly.GetExecutingAssembly(); // Get the current assembly
+var types = assembly.GetTypes(); // Get all types in the assembly
+
+#endregion
+
+#region Reflection with Type type
+
+MyClass m1 = new MyClass();
+Type type = m1.GetType(); // Get the type of the object
+Type type2 = typeof(MyClass); // Get the type of the class
+
+#endregion
+
+Console.WriteLine();
+
+
+
+class MyClass
+{
+    public int MyProperty { get; set; }
+    public int MyProperty2 { get; set; }
+    public string MyProperty3 { get; set; }
+    int MyProperty4 { get; set; }
+    int MyField;
+    public void MyMethod() { }
+}
