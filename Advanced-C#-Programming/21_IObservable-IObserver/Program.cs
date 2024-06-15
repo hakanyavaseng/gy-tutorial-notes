e1679ebd0e3bc40960c8c131416dfd08732edaf3
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-15 18:29:24 +0300

    ISubject interface covered.

--- Advanced-C#-Programming/21_IObservable-IObserver/Program.cs
+++ Advanced-C#-Programming/21_IObservable-IObserver/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine();
+﻿using System.Reactive;
+using System.Reactive.Subjects;
+
+Console.WriteLine();
 
 #region Practice 1
-
+/*
 MyObservable observable = new();
 
 using var sub1 = observable.Subscribe(new MyObserver("1"));
@@ -58,5 +61,52 @@ class MyObserver(string observerName) : IObserver<int>
         Console.WriteLine($"{observerName} Value: {value}");
     }
 }
+*/
+#endregion
+
+#region ISubject
+
+ISubject<int> subject = new Subject<int>();
+
+MyObserver observer1 = new("A");
+MyObserver observer2 = new("B");
+MyObserver observer3 = new("C");
+
+var aSub = subject.Subscribe(observer1);
+var bSub = subject.Subscribe(observer2);
+var cSub = subject.Subscribe(observer3);
+
+subject.OnNext(10);
+
+bSub.Dispose();
+
+subject.OnNext(20);
+subject.OnNext(30);
+
+class MyObserver(string observerName) : IObserver<int>
+{
+    public void OnCompleted()
+    {
+        Console.WriteLine($"{observerName} Completed");
+    }
+
+    public void OnError(Exception error)
+    {
+        Console.WriteLine($"{observerName} Error: {error.Message}");
+    }
+
+    public void OnNext(int value)
+    {
+        Console.WriteLine($"{observerName} Value: {value}");
+    }
+}
+
+
+
+
+
+
+
+
 
 #endregion
