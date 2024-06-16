63d90dc633715f8bd62e7cb1b60a4c15af091c05
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-16 14:53:50 +0300

    Extension methods covered.

---
+++ Advanced-C#-Programming/25_Extension-Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine();
+
+#region Extension Methods 
+
+MyClass m = new();
+m.Print();
+
+static class MyExtensions
+{
+    public static void Print(this MyClass myClass)
+    {
+        Console.WriteLine("Print() method called");
+    }
+}
+
+
+#endregion
+
+class MyClass
+{
+
+}
