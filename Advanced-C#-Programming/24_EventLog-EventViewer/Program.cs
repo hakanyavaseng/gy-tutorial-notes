6fb8a350e533f77404539a9a48bec7d5aecb4d16
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-16 14:36:19 +0300

    EventLog covered.

---
+++ Advanced-C#-Programming/24_EventLog-EventViewer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine();
+
+#region EventLog
+const string eventLogSource = "EventLog-EventViewer";
+
+string logName = "ApplicationLog";
+
+if(!EventLog.SourceExists(eventLogSource))
+{
+    EventLog.CreateEventSource(eventLogSource, logName);
+    Console.WriteLine($"Event Source '{eventLogSource}' created for log '{logName}'");
+}
+
+string logMessage = $"This is a test message, Event Log: {DateTime.UtcNow}";
+EventLog.WriteEntry(eventLogSource, logMessage, EventLogEntryType.Information);
+Console.WriteLine($"Event Log entry written: {logMessage}");
+Process.Start("eventvwr.exe");
+#endregion
