a99f834f47ef08fc87f3761cf4ed571dfbabb44f
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-06-16 21:54:01 +0300

    Static abstract members covered with interface and generic types 1/2

---
+++ Advanced-C#-Programming/26_Debugger-Display/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+
+Person person = new()
+{
+    Name = "Hakan",
+    Age = 23,
+    City = "Eskisehir",
+    Region = "Anatolia"
+};
+
+var p = person;
+
+Console.WriteLine();
+
+#region DebuggerDisplay Attribute
+//DebuggerDisplay Attribute gives opportunity to customize the display of the object in the debugger. So, the developer can see the properties of the object in the debugger window without expanding the object.
+
+[DebuggerDisplay("Name = {Name}, Age = {Age}, Şehir = {City}, Bölge = {Region}")]
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+}
+#endregion
---
+++ Advanced-C#-Programming/27_Static-Abstract-Members/Program.cs
@@ -0,0 +1,78 @@
+﻿Console.WriteLine();
+
+/*
+interface IX
+{
+    abstract void B();  // By default compiler will add abstract keyword
+    virtual void A() { } // By default compiler will add virtual keyword to default implementation
+}
+
+class X : IX
+{
+    public void B()
+    {
+        throw new NotImplementedException();
+    }
+}
+*/
+#region Static Abstract Members in Interface
+/*
+IX.C(); // C Default Implementation.
+//IX.B(); // A static virtual or abstract interface member can be accesed only on a type parameter.
+X.A(); // X.A
+X.B(); // X.B
+
+interface IX
+{
+    static abstract void A(); // Normal implementation can be static abstract
+    static abstract void B();
+    static void C() // static keyword can be used in default implementation
+    {
+        Console.WriteLine("C Default Implementation.");
+
+    }
+}
+
+class X : IX
+{
+    public static void A()
+    {
+        Console.WriteLine("X.A");
+        
+    }
+
+    public static void B()
+    {
+        Console.WriteLine("X.B");
+       
+    }
+}
+*/
+#endregion
+
+#region Static Abstract Members in Interface with Generic Types
+/*
+class X<T> where T : IX
+{
+    public X()
+    {
+        T.A();
+        T.B();
+        //T.C(); // Gives error, because with generic type only static abstract members can be accessed.
+    }
+}
+
+
+interface IX
+{
+    static abstract void A();
+    static abstract void B();
+    static void C()
+    {
+        Console.WriteLine("C Default Implementation.");
+
+    }
+}
+*/
+#endregion
+
